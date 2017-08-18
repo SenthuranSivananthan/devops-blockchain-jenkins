8430f3b9849c09ea628bab7389f56b3bb931bf69
Senthuran Sivananthan <senthuran.sivananthan@microsoft.com>
2017-08-17 22:19:08 -0400

    Modified account retrivable based on path variable

--- services/src/main/java/com/microsoft/blockchain/services/controllers/AccountController.java
+++ services/src/main/java/com/microsoft/blockchain/services/controllers/AccountController.java
@@ -1,10 +1,8 @@
 package com.microsoft.blockchain.services.controllers;
 
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microsoft.blockchain.services.models.Account;
@@ -17,14 +15,14 @@ import com.microsoft.blockchain.services.models.FundTransferStatus;
 @RestController
 @RequestMapping("/api")
 public class AccountController {
-	@RequestMapping("/account")
-	public Account getAccount(@RequestParam("id") String address) {
+	@RequestMapping("/account/{address}")
+	public Account getAccount(@PathVariable("address") String address) {
 		return new Account(address, "Senthuran", 1000);
 	}
-	
-	@RequestMapping(method = RequestMethod.POST, path="/account/{sourceAddress}/transfer/{destinationAddress}/{amount}")
-	public FundTransferStatus transferFunds(@PathVariable("sourceAddress") String sourceAddress, @PathVariable("destinationAddress") String destinationAddress, @PathVariable("amount") double amount)
-	{
+
+	@RequestMapping(method = RequestMethod.POST, path = "/account/{sourceAddress}/transfer/{destinationAddress}/{amount}")
+	public FundTransferStatus transferFunds(@PathVariable("sourceAddress") String sourceAddress,
+			@PathVariable("destinationAddress") String destinationAddress, @PathVariable("amount") double amount) {
 		return new FundTransferStatus(sourceAddress, destinationAddress, amount, true);
 	}
 }
